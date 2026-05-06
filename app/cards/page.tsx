dbd62a4dd15864a360144843cfd1fdb758c8fa92
Ricardo <ricardopurba090606@gmail.com>
2026-05-06 22:05:15 +0800

    cards

---
+++ app/cards/page.tsx
@@ -0,0 +1,51 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[#dfe3ea] p-3">
+
+      <div className="flex gap-5">
+
+        <div className="w-[250px] h-[250px] bg-white rounded-[8px] mt-5 p-3">
+          <div className="w-full h-[180px] bg-blue-100 rounded-2xl p-4">
+            <h1 className="text-2xl font-bold">Web Design</h1>
+
+            <p className="mt-2 text-white">
+              Creates engaging, user friendly websites.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-[250px] h-[250px] bg-white rounded-[8px] mt-5 p-3">
+          <div className="w-full h-[180px] bg-blue-100 rounded-2xl p-4">
+            <h1 className="text-2xl font-bold">UI Design</h1>
+
+            <p className="mt-2 text-white">
+              creates inpactful visual and branding.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-[250px] h-[250px] bg-white rounded-[8px] mt-5 p-3">
+          <div className="w-full h-[180px] bg-blue-100 rounded-2xl p-4">
+            <h1 className="text-2xl font-bold">Frontend</h1>
+
+            <p className="mt-2 text-white">
+              Build functional and scalable solutions.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-[250px] h-[250px] bg-white rounded-[8px] mt-5 p-3">
+          <div className="w-full h-[180px] bg-blue-100 rounded-2xl p-4">
+            <h1 className="text-2xl font-bold">Branding</h1>
+
+            <p className="mt-2 text-white">
+              Creative visual identity.
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
