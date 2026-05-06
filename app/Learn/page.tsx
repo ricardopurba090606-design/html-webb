8561f0dc6926e7b585e4d2d59d536786f041d8f2
Ricardo <ricardopurba090606@gmail.com>
2026-05-06 12:25:20 +0800

    first commit

---
+++ app/Learn/page.tsx
@@ -0,0 +1,17 @@
+export default function Learn() {
+  return (
+    <div className="bg-white h-screen flex text-blue-950">
+      <div className="bg-purple-300 h-72 w-96 p-8 flex flex-col justify-between rounded-4x">
+        <div className="bg-orange-300 flex gap-4 p-3">
+          <img src="/palette.svg" alt="icon Palette" />
+          <p>Design</p>
+        </div>
+
+        <div className="bg-blue-300 mt-4 p-3">
+          <h1>Figma</h1>
+          <p>8 hours ago</p>
+        </div>
+      </div>
+    </div>
+  );
+}
