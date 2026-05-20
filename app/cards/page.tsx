be1770eb4195f21284c44593df8e759066b1df88
Ricardo <ricardopurba090606@gmail.com>
2026-05-20 21:59:25 +0800

    update

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -1,51 +1,33 @@
 export default function App() {
   return (
-    <div className="min-h-screen bg-[#dfe3ea] p-3">
+    <main className="h-screen flex">
+      <section className="w-72 bg-gray-500 p-4 h-full">
+        <div className="flex justify-between items-center">
+          
+          <div className="flex gap-3 items-center">
 
-      <div className="flex gap-5">
+            <div className="bg-blue-600 px-3 py-2 rounded-xl">
+              <img
+                src="/wallet.svg"
+                alt="wallet"
+                className="w-6 h-6 invert"
+              />
+            </div>
 
-        <div className="w-[250px] h-[250px] bg-white rounded-[8px] mt-5 p-3">
-          <div className="w-full h-[180px] bg-blue-100 rounded-2xl p-4">
-            <h1 className="text-2xl font-bold">Web Design</h1>
+            <div>
+              <h1 className="font-bold text-white">
+                Acme Inc
+              </h1>
 
-            <p className="mt-2 text-white">
-              Creates engaging, user friendly websites.
-            </p>
+              <p className="text-sm text-gray-200">
+                Enterprise
+              </p>
+            </div>
+
           </div>
+
         </div>
-
-        <div className="w-[250px] h-[250px] bg-white rounded-[8px] mt-5 p-3">
-          <div className="w-full h-[180px] bg-blue-100 rounded-2xl p-4">
-            <h1 className="text-2xl font-bold">UI Design</h1>
-
-            <p className="mt-2 text-white">
-              creates inpactful visual and branding.
-            </p>
-          </div>
-        </div>
-
-        <div className="w-[250px] h-[250px] bg-white rounded-[8px] mt-5 p-3">
-          <div className="w-full h-[180px] bg-blue-100 rounded-2xl p-4">
-            <h1 className="text-2xl font-bold">Frontend</h1>
-
-            <p className="mt-2 text-white">
-              Build functional and scalable solutions.
-            </p>
-          </div>
-        </div>
-
-        <div className="w-[250px] h-[250px] bg-white rounded-[8px] mt-5 p-3">
-          <div className="w-full h-[180px] bg-blue-100 rounded-2xl p-4">
-            <h1 className="text-2xl font-bold">Branding</h1>
-
-            <p className="mt-2 text-white">
-              Creative visual identity.
-            </p>
-          </div>
-        </div>
-
-      </div>
-
-    </div>
+      </section>
+    </main>
   );
 }
